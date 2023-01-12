09e6bd828bcb3fa5916d12e512126e90f6889b86
Căbău Natan <110463937+CNatiDev@users.noreply.github.com>
2023-01-12 18:22:49 +0200

    Update Respawn.cs

--- Respawn.cs
+++ Respawn.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] ObjToRespawn;
+    public GameObject[] PointForRespawn;
+    //The RespawnObject function is for respawning only one desired object
+    public void RespawnObject(int i)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (ObjToRespawn[i]!=null&&PointForRespawn[i]!=null)
+        {
+            ObjToRespawn[i].transform.position = PointForRespawn[i].transform.position;
+            ObjToRespawn[i].transform.localRotation = Quaternion.EulerAngles(PointForRespawn[i].transform.localRotation.x, PointForRespawn[i].transform.localRotation.y, PointForRespawn[i].transform.localRotation.z);
+        }
     }
 }
